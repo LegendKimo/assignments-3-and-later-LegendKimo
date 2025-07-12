3e415cfa32d88e4a5c4bce680d6db51b8dac6e9d
kareem khaled <k.kh1994@yahoo.com>
2025-07-12 15:09:03 +0300

    WIP on main
    fix for solution Assignment4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include<errno.h>
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
@@ -14,6 +14,35 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success=false;
+    fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- threadfunc started\n");
+        fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- start_thread_obtaining_mutex ID of thread is %lu  \n",pthread_self());
+
+        fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- wait to obtain %d\n",thread_func_args->wait_to_obtain_ms);
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if(rc!=0){
+        fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- unable to lock the mutex \n");
+        //fflush(stdout); 
+           // thread_func_args->thread_complete_success=false;
+               return thread_param;
+
+    }
+    fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- wait to release %d\n",thread_func_args->wait_to_release_ms);
+    usleep(thread_func_args->wait_to_release_ms*1000);
+   int rw = pthread_mutex_unlock(thread_func_args->mutex); 
+    if(rw!=0){
+        fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- unable to unlock the mutex \n");
+        //fflush(stdout); 
+            thread_func_args->thread_complete_success=false;
+                return thread_param;
+    }
+    else{
+            fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- thread executed succesfully \n");
+           // fflush(stdout); 
+    } 
+        thread_func_args->thread_complete_success=true;
     return thread_param;
 }
 
@@ -28,6 +57,35 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+     struct thread_data* data = malloc(sizeof(struct thread_data));
+     if (data == NULL) {  // ✅ ADDED: Null check
+    fprintf(stderr, "Debug: malloc failed\n");
     return false;
+}
+        fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- start_thread_obtaining_mutex started\n");
+     data->wait_to_obtain_ms=wait_to_obtain_ms;
+     data->wait_to_release_ms=wait_to_release_ms;
+     data->mutex= mutex;
+    //pthread_mutex_init(&mutex,NULL);
+    int ret =pthread_create(thread,NULL,threadfunc,data);
+     fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- pthread_create ret of thread is %d  \n",ret);
+
+    if (ret!=0)
+    {
+        errno = ret;
+       // perror("pthread_create");
+       // fflush(stdout); 
+                fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- unable to create the thread \n");
+       free(data);
+        return false;
+    }
+    // if(data->thread_complete_success==true){
+    //     //free(data);
+    //             fprintf(stderr,"Debuuuuuuuuuuuuuuuuuuuuuuug------------- thread_complete_success==true\n");
+    //     return false;
+    // }
+
+
+    return true;
 }
 
